52ca2ae75a15ece719c6c2ad373b018adacd9628
Felipe Corr?a da Silva Sanches <juca@members.fsf.org>
2010-10-22 21:11:45 -0300

    initial code commit

---
+++ Firefox/xpi.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+zip fontsdownloader-svn.xpi \
+		content/overlay.xul \
+		content/configure.xul \
+		content/fontpreview.xul \
+		content/fontsdownloader.js \
+		content/utils.js \
+		defaults/preferences/webfontdownloader.js \
+		skin/fontpreview.css \
+		install.rdf \
+		LICENSE.TXT \
+		chrome.manifest
+
