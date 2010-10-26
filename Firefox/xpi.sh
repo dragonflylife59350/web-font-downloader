f1f28e6cee1ff5a920752b92665fbc9ab1462f0f
Felipe Corr?a da Silva Sanches <juca@members.fsf.org>
2010-10-26 06:47:48 -0300

    implementing font listing dialog DTA-style

--- Firefox/xpi.sh
+++ Firefox/xpi.sh
@@ -3,6 +3,7 @@ zip fontsdownloader-svn.xpi \
 		content/overlay.xul \
 		content/configure.xul \
 		content/fontpreview.xul \
+		content/fontselector.xul \
 		content/fontsdownloader.js \
 		content/utils.js \
 		defaults/preferences/webfontdownloader.js \
