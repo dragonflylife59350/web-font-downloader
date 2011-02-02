c12399a172c211e524f480f26b3145a5c0722e6f
Dave Crossland <d.crossland@gmail.com>
2011-02-02 00:32:44 +0000

    Reworded parts of the UI (font formats in table, anchor texts,etc)

---
+++ Firefox/xpi.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+zip web-font-downloader-svn.xpi \
+		content/overlay.xul \
+		content/configure.xul \
+		content/fontpreview.xul \
+		content/fontselector.xul \
+		content/fontsdownloader.js \
+		content/utils.js \
+		defaults/preferences/webfontdownloader.js \
+		skin/fontpreview.css \
+		skin/fontselector.css \
+		skin/statusbarOverlay.css \
+		skin/font-icon.png \
+		skin/webfontdownloader-16x16.png \
+		skin/webfontdownloader-32x32.png \
+		skin/webfontdownloader-16x16.svg \
+		skin/webfontdownloader-32x32.svg \
+		icon.png \
+		install.rdf \
+		LICENSE.TXT \
+		chrome.manifest
+
