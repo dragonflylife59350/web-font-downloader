908e023e925315dbd23444ab627864bfcc046822
Dave Crossland <d.crossland@gmail.com>
2011-02-02 14:08:18 +0000

    Changing non-previewable fonts invisible

---
+++ Firefox/xpi.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+zip web-font-downloader-svn.xpi \
+		content/overlay.xul \
+		content/configure.xul \
+		content/fontpreview.xul \
+		content/fontselector.xul \
+		content/fontsdownloader.js \
+		content/utils.js \
+		defaults/preferences/webfontdownloader.js \
+		skin/fontpreview.css \
+		skin/fontselector.css \
+		skin/statusbarOverlay.css \
+		skin/font-icon.png \
+		skin/webfontdownloader-16x16.png \
+		skin/webfontdownloader-16x16-unchecked.png \
+		skin/webfontdownloader-32x32.png \
+		skin/webfontdownloader-16x16.svg \
+		skin/webfontdownloader-16x16-unchecked.svg \
+		skin/webfontdownloader-32x32.svg \
+		icon.png \
+		install.rdf \
+		LICENSE.TXT \
+		chrome.manifest
+
